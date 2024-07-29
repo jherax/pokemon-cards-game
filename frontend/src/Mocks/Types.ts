4920442f210f3aa6cb552d525db875dd3cb3e34c
David Rivera <jherax@gmail.com>
2024-07-28 19:50:28 -0500

    chore: Added packages, images and data-mocks for Pokemon

---
+++ frontend/src/Mocks/Types.ts
@@ -0,0 +1,17 @@
+const TYPES = {
+  types: [
+    {bg: '#A0A29F', img: '/static/media/Colorless.svg', name: 'Colorless'},
+    {bg: '#1e3439', img: '/static/media/Darkness.svg', name: 'Darkness'},
+    {bg: '#796b44', img: '/static/media/Dragon.svg', name: 'Dragon'},
+    {bg: '#EE90E6', img: '/static/media/Fairy.svg', name: 'Fairy'},
+    {bg: '#8e431b', img: '/static/media/Fighting.svg', name: 'Fighting'},
+    {bg: '#9c2221', img: '/static/media/Fire.svg', name: 'Fire'},
+    {bg: '#5FBD58', img: '/static/media/Grass.svg', name: 'Grass'},
+    {bg: '#F2D94E', img: '/static/media/Lightning.svg', name: 'Lightning'},
+    {bg: '#5b5654', img: '/static/media/Metal.svg', name: 'Metal'},
+    {bg: '#724b8d', img: '/static/media/Psychic.svg', name: 'Psychic'},
+    {bg: '#539DDF', img: '/static/media/Water.svg', name: 'Water'},
+  ],
+};
+
+export default TYPES;
--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,1 +1,8 @@
+/* eslint-disable spaced-comment */
+
 /// <reference types="react-scripts" />
+
+/**
+ * module 'react-jss'
+ * @see https://github.com/cssinjs/jss/issues/1203#issuecomment-547983799
+ */
