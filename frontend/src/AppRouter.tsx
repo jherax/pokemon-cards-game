dbde6c684537497e6004011d60752575ed249c5e
David Rivera <jherax@gmail.com>
2024-07-30 14:12:02 -0500

    feat: Added the Navigation bar component to the router

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -5,17 +5,30 @@ import {
   Route,
 } from 'react-router-dom';
 
+import NavigationBar from './Components/Navigation/NavigationBar';
 import Error404 from './Pages/Error404';
 import PokemonCard from './Pages/PokemonCard';
 import PokemonCards from './Pages/PokemonCards';
 import PokemonTypes from './Pages/PokemonTypes';
 
 /**
+ * @see https://www.dhiwise.com/post/the-power-of-createbrowserrouter-optimizing-your-react-app
+ */
+function AppLayout() {
+  return (
+    <div>
+      <NavigationBar />
+      <Outlet /> {/* Nested routes render here */}
+    </div>
+  );
+}
+
+/**
  * @see https://reactrouter.com/en/main/route/route
  */
 const AppRouter = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Outlet />} errorElement={<Error404 />}>
+    <Route path='/' element={<AppLayout />} errorElement={<Error404 />}>
       <Route path='' element={<PokemonTypes />} />
       <Route path='/:type' element={<PokemonCards />} />
       <Route path='/:type/:id' element={<PokemonCard />} />
