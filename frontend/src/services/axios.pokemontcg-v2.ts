ce03c3c6b6a6a9375ae2ae086f7d9451acdce682
David Rivera <jherax@gmail.com>
2024-08-07 11:41:30 -0500

    feat: Added a new axios instance for pokemontcg v2 API

---
+++ frontend/src/services/axios.pokemontcg-v2.ts
@@ -0,0 +1,29 @@
+import Axios, {type AxiosRequestConfig} from 'axios';
+
+import config from '../config/app.cfg';
+
+const POKEMON_API_URL = 'https://api.pokemontcg.io/v2';
+
+// This Header is required only for PROD environments
+const instanceConfig: AxiosRequestConfig = config.POKEMON_API_KEY
+  ? {
+      headers: {
+        'X-Api-Key': `${config.POKEMON_API_KEY}`,
+      },
+    }
+  : {};
+
+const instance = Axios.create({
+  baseURL: POKEMON_API_URL,
+  ...instanceConfig,
+});
+
+/** @see https://blog.logrocket.com/using-axios-set-request-headers/#setting-conditional-headers */
+instance.interceptors.request.use(
+  // Auth and token validation should be handled here:
+  // config.headers['Authorization'] = `Bearer ${TOKEN}`
+  config => config,
+  error => Promise.reject(error),
+);
+
+export const pokemonService = instance;
